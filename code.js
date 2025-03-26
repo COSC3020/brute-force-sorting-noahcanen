ff761898a6366439a5cb4591d1e6bcccf0608235
noahcanen <ncanen@uwyo.edu>
2025-03-26 17:38:50 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,31 +1,31 @@
 function permutationSort(x) {
     num = 0
-    if (sorted(x) == true) {return num}
+    if (sorted(x) == true) {
+        return num
+    }
     num = permutation(x,0,num)
 
     return num
 }
 function permutation(x, a,num) {
-    
-
     for (var b = a; b < x.length; b++) {
         if (sorted(x) == true) {
             return num
         }
         num++
         [x[a], x[b]] = [x[b], x[a]]
-        permutation(x, a + 1)
+        num = permutation(x, a + 1,num)+num
         if (sorted(x) == true) {
             return num
         }
         [x[b], x[a]] = [x[a], x[b]]
     }
+    return num
     
 }
+
 function sorted(x){
-    console.log("sorted")
     for (let i = 0; i < x.length; i++) {
-        console.log(x[i] , x[i+1],x)
         if (x[i] > x[i+1]){
             return false
         }
