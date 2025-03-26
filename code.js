85f90d1a3099a1978934e7683022df457339354f
noahcanen <ncanen@uwyo.edu>
2025-03-26 17:01:16 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,34 @@
-function permutationSort(a) {
-    return 0;
+function permutationSort(x) {
+    num = 0
+    num = permutation(x,0,num)
+
+    return num
+}
+function permutation(x, a,num) {
+    
+
+    for (var b = a; b < x.length; b++) {
+        if (sorted(x) == true) {
+            return num
+        }
+        num++
+        [x[a], x[b]] = [x[b], x[a]]
+        permutation(x, a + 1)
+        if (sorted(x) == true) {
+            return num
+        }
+        [x[b], x[a]] = [x[a], x[b]]
+    }
+    
+}
+
+function sorted(x){
+    console.log("sorted")
+    for (let i = 0; i < x.length; i++) {
+        console.log(x[i] , x[i+1],x)
+        if (x[i] > x[i+1]){
+            return false
+        }
+    }
+    return true
 }
